dc7ef619e540dfbe9057c9456707d3400d667c10
Jorge AB <ing.jorge.villa@hotmail.com>
2025-04-15 23:25:36 +0000

    Ignorar archivos .js y limpiar rastreo

--- src/navbar.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Link } from "react-router-dom";
-import "./css/navbar.css";
-const Navbar = () => {
-    return (_jsx("nav", { className: "", children: _jsx("div", { className: "", children: _jsx("div", { className: "", children: _jsxs("div", { className: "", children: [_jsx("div", { className: "", children: _jsx("span", { className: "", children: "Sistema de Reportes" }) }), _jsxs("div", { className: "", children: [_jsx(Link, { to: "/crear", className: "", children: "Hacer Reporte" }), _jsx(Link, { to: "/ver", className: "", children: "Ver Reportes" }), _jsx(Link, { to: "/modificar", className: "", children: "Buscar y modificar" })] })] }) }) }) }));
-};
-export default Navbar;
