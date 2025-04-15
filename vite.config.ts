51cd059a28e8b8b9259fa564e31f02e9dd471f87
CodeSandbox <support@codesandbox.io>
2025-04-15 20:13:39 +0000

    Sandbox snapshot commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+// vite.config.js or vite.config.ts
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
-  
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ["463n2w-5173.csb.app"],
+  },
+});
